fb14e004c53ac648af00c55e09db3835938cf6a8
Artyomsheq <artimushka@gmail.com>
2023-07-03 22:12:22 +0600

    second page

---
+++ constants/productCard.js
@@ -0,0 +1,54 @@
+export const productCard = [
+    {
+        id: 1,
+        img: "/newsExtension.png",
+        title: "Конкурс на лучший сторителлинг по открытию IT-бизнеса в Казахстане продлевается до 20 июля",
+        textImg: "/cardText.svg",
+        text: "0",
+        shareImg: "/cardShare.svg",
+        saveImg: "/cardSave.svg",
+        date: "21 июня 2023 г. 18:01",
+        fixImg: "/cardFix.svg",
+        viewedImg: "/cardViewed.svg",
+        viewed: "537"
+    },
+    {
+        id: 2,
+        img: "/newsPeoples.png",
+        title: "Участники Silkway Accelerator совокупно привлекли $7,5 млн инвестиции",
+        textImg: "/cardText.svg",
+        text: "0",
+        shareImg: "/cardShare.svg",
+        saveImg: "/cardSave.svg",
+        date: "29 июня 2023 г. 9:20",
+        fixImg: "/cardFix.svg",
+        viewedImg: "/cardViewed.svg",
+        viewed: "156"
+    },
+    {
+        id: 3,
+        img: "/newsSilk.png",
+        title: "В Astana Hub идет набор на новый – четвертый поток программы Silkway Accelerator",
+        textImg: "/cardText.svg",
+        text: "0",
+        shareImg: "/cardShare.svg",
+        saveImg: "/cardSave.svg",
+        date: "27 июня 2023 г. 21:43",
+        fixImg: "/cardFix.svg",
+        viewedImg: "/cardViewed.svg",
+        viewed: "167"
+    },
+    {
+        id: 4,
+        img: "/newsMeta.png",
+        title: "Виртуальная ярмарка вакансий пройдет в Казахстане",
+        textImg: "/cardText.svg",
+        text: "0",
+        shareImg: "/cardShare.svg",
+        saveImg: "/cardSave.svg",
+        date: "27 июня 2023 г. 21:43",
+        fixImg: "/cardFix.svg",
+        viewedImg: "/cardViewed.svg",
+        viewed: "56"
+    },
+]
